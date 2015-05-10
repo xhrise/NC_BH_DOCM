05bfd71d27ce72ab02e3a7b88a224fbaf665a74a
xhrise <xhrise@gmail.com>
2015-05-10 17:40:36 +0800

    元数据发布,生成代码,创建对象数据库表

---
+++ NC_BH_DOCM/docmng/src/public/nc/vo/docmng/doctype/DocType.java
@@ -0,0 +1,743 @@
+package nc.vo.docmng.doctype;
+
+import nc.vo.pub.IVOMeta;
+import nc.vo.pub.SuperVO;
+import nc.vo.pub.lang.UFBoolean;
+import nc.vo.pub.lang.UFDate;
+import nc.vo.pub.lang.UFDateTime;
+import nc.vo.pub.lang.UFDouble;
+import nc.vo.pubapp.pattern.model.meta.entity.vo.VOMetaFactory;
+
+public class DocType extends SuperVO {
+/**
+*编码
+*/
+public String code;
+/**
+*创建时间
+*/
+public static final String CREATIONTIME="creationtime";
+/**
+*创建人
+*/
+public static final String CREATOR="creator";
+/**
+*自定义项1
+*/
+public static final String DEF1="def1";
+/**
+*自定义项10
+*/
+public static final String DEF10="def10";
+/**
+*自定义项11
+*/
+public static final String DEF11="def11";
+/**
+*自定义项12
+*/
+public static final String DEF12="def12";
+/**
+*自定义项13
+*/
+public static final String DEF13="def13";
+/**
+*自定义项14
+*/
+public static final String DEF14="def14";
+/**
+*自定义项15
+*/
+public static final String DEF15="def15";
+/**
+*自定义项16
+*/
+public static final String DEF16="def16";
+/**
+*自定义项17
+*/
+public static final String DEF17="def17";
+/**
+*自定义项18
+*/
+public static final String DEF18="def18";
+/**
+*自定义项19
+*/
+public static final String DEF19="def19";
+/**
+*自定义项2
+*/
+public static final String DEF2="def2";
+/**
+*自定义项20
+*/
+public static final String DEF20="def20";
+/**
+*自定义项3
+*/
+public static final String DEF3="def3";
+/**
+*自定义项4
+*/
+public static final String DEF4="def4";
+/**
+*自定义项5
+*/
+public static final String DEF5="def5";
+/**
+*自定义项6
+*/
+public static final String DEF6="def6";
+/**
+*自定义项7
+*/
+public static final String DEF7="def7";
+/**
+*自定义项8
+*/
+public static final String DEF8="def8";
+/**
+*自定义项9
+*/
+public static final String DEF9="def9";
+/**
+*修改时间
+*/
+public static final String MODIFIEDTIME="modifiedtime";
+/**
+*修改人
+*/
+public static final String MODIFIER="modifier";
+/**
+*上级对象
+*/
+public static final String PID="pid";
+/**
+*主键
+*/
+public String pk_doctype;
+/**
+*集团
+*/
+public static final String PK_GROUP="pk_group";
+/**
+*组织
+*/
+public static final String PK_ORG="pk_org";
+/**
+*组织版本
+*/
+public static final String PK_ORG_V="pk_org_v";
+/**
+*时间戳
+*/
+public UFDateTime ts;
+/**
+*名称
+*/
+public String vcode;
+/**
+*备注
+*/
+public static final String VMEMO="vmemo";
+/** 
+* 获取编码
+*
+* @return 编码
+*/
+public String getCode () {
+return this.code;
+ } 
+
+/** 
+* 设置编码
+*
+* @param code 编码
+*/
+public void setCode ( String code) {
+this.code=code;
+ } 
+
+/** 
+* 获取创建时间
+*
+* @return 创建时间
+*/
+public UFDateTime getCreationtime () {
+return (UFDateTime) this.getAttributeValue( DocType.CREATIONTIME);
+ } 
+
+/** 
+* 设置创建时间
+*
+* @param creationtime 创建时间
+*/
+public void setCreationtime ( UFDateTime creationtime) {
+this.setAttributeValue( DocType.CREATIONTIME,creationtime);
+ } 
+
+/** 
+* 获取创建人
+*
+* @return 创建人
+*/
+public String getCreator () {
+return (String) this.getAttributeValue( DocType.CREATOR);
+ } 
+
+/** 
+* 设置创建人
+*
+* @param creator 创建人
+*/
+public void setCreator ( String creator) {
+this.setAttributeValue( DocType.CREATOR,creator);
+ } 
+
+/** 
+* 获取自定义项1
+*
+* @return 自定义项1
+*/
+public String getDef1 () {
+return (String) this.getAttributeValue( DocType.DEF1);
+ } 
+
+/** 
+* 设置自定义项1
+*
+* @param def1 自定义项1
+*/
+public void setDef1 ( String def1) {
+this.setAttributeValue( DocType.DEF1,def1);
+ } 
+
+/** 
+* 获取自定义项10
+*
+* @return 自定义项10
+*/
+public String getDef10 () {
+return (String) this.getAttributeValue( DocType.DEF10);
+ } 
+
+/** 
+* 设置自定义项10
+*
+* @param def10 自定义项10
+*/
+public void setDef10 ( String def10) {
+this.setAttributeValue( DocType.DEF10,def10);
+ } 
+
+/** 
+* 获取自定义项11
+*
+* @return 自定义项11
+*/
+public String getDef11 () {
+return (String) this.getAttributeValue( DocType.DEF11);
+ } 
+
+/** 
+* 设置自定义项11
+*
+* @param def11 自定义项11
+*/
+public void setDef11 ( String def11) {
+this.setAttributeValue( DocType.DEF11,def11);
+ } 
+
+/** 
+* 获取自定义项12
+*
+* @return 自定义项12
+*/
+public String getDef12 () {
+return (String) this.getAttributeValue( DocType.DEF12);
+ } 
+
+/** 
+* 设置自定义项12
+*
+* @param def12 自定义项12
+*/
+public void setDef12 ( String def12) {
+this.setAttributeValue( DocType.DEF12,def12);
+ } 
+
+/** 
+* 获取自定义项13
+*
+* @return 自定义项13
+*/
+public String getDef13 () {
+return (String) this.getAttributeValue( DocType.DEF13);
+ } 
+
+/** 
+* 设置自定义项13
+*
+* @param def13 自定义项13
+*/
+public void setDef13 ( String def13) {
+this.setAttributeValue( DocType.DEF13,def13);
+ } 
+
+/** 
+* 获取自定义项14
+*
+* @return 自定义项14
+*/
+public String getDef14 () {
+return (String) this.getAttributeValue( DocType.DEF14);
+ } 
+
+/** 
+* 设置自定义项14
+*
+* @param def14 自定义项14
+*/
+public void setDef14 ( String def14) {
+this.setAttributeValue( DocType.DEF14,def14);
+ } 
+
+/** 
+* 获取自定义项15
+*
+* @return 自定义项15
+*/
+public String getDef15 () {
+return (String) this.getAttributeValue( DocType.DEF15);
+ } 
+
+/** 
+* 设置自定义项15
+*
+* @param def15 自定义项15
+*/
+public void setDef15 ( String def15) {
+this.setAttributeValue( DocType.DEF15,def15);
+ } 
+
+/** 
+* 获取自定义项16
+*
+* @return 自定义项16
+*/
+public String getDef16 () {
+return (String) this.getAttributeValue( DocType.DEF16);
+ } 
+
+/** 
+* 设置自定义项16
+*
+* @param def16 自定义项16
+*/
+public void setDef16 ( String def16) {
+this.setAttributeValue( DocType.DEF16,def16);
+ } 
+
+/** 
+* 获取自定义项17
+*
+* @return 自定义项17
+*/
+public String getDef17 () {
+return (String) this.getAttributeValue( DocType.DEF17);
+ } 
+
+/** 
+* 设置自定义项17
+*
+* @param def17 自定义项17
+*/
+public void setDef17 ( String def17) {
+this.setAttributeValue( DocType.DEF17,def17);
+ } 
+
+/** 
+* 获取自定义项18
+*
+* @return 自定义项18
+*/
+public String getDef18 () {
+return (String) this.getAttributeValue( DocType.DEF18);
+ } 
+
+/** 
+* 设置自定义项18
+*
+* @param def18 自定义项18
+*/
+public void setDef18 ( String def18) {
+this.setAttributeValue( DocType.DEF18,def18);
+ } 
+
+/** 
+* 获取自定义项19
+*
+* @return 自定义项19
+*/
+public String getDef19 () {
+return (String) this.getAttributeValue( DocType.DEF19);
+ } 
+
+/** 
+* 设置自定义项19
+*
+* @param def19 自定义项19
+*/
+public void setDef19 ( String def19) {
+this.setAttributeValue( DocType.DEF19,def19);
+ } 
+
+/** 
+* 获取自定义项2
+*
+* @return 自定义项2
+*/
+public String getDef2 () {
+return (String) this.getAttributeValue( DocType.DEF2);
+ } 
+
+/** 
+* 设置自定义项2
+*
+* @param def2 自定义项2
+*/
+public void setDef2 ( String def2) {
+this.setAttributeValue( DocType.DEF2,def2);
+ } 
+
+/** 
+* 获取自定义项20
+*
+* @return 自定义项20
+*/
+public String getDef20 () {
+return (String) this.getAttributeValue( DocType.DEF20);
+ } 
+
+/** 
+* 设置自定义项20
+*
+* @param def20 自定义项20
+*/
+public void setDef20 ( String def20) {
+this.setAttributeValue( DocType.DEF20,def20);
+ } 
+
+/** 
+* 获取自定义项3
+*
+* @return 自定义项3
+*/
+public String getDef3 () {
+return (String) this.getAttributeValue( DocType.DEF3);
+ } 
+
+/** 
+* 设置自定义项3
+*
+* @param def3 自定义项3
+*/
+public void setDef3 ( String def3) {
+this.setAttributeValue( DocType.DEF3,def3);
+ } 
+
+/** 
+* 获取自定义项4
+*
+* @return 自定义项4
+*/
+public String getDef4 () {
+return (String) this.getAttributeValue( DocType.DEF4);
+ } 
+
+/** 
+* 设置自定义项4
+*
+* @param def4 自定义项4
+*/
+public void setDef4 ( String def4) {
+this.setAttributeValue( DocType.DEF4,def4);
+ } 
+
+/** 
+* 获取自定义项5
+*
+* @return 自定义项5
+*/
+public String getDef5 () {
+return (String) this.getAttributeValue( DocType.DEF5);
+ } 
+
+/** 
+* 设置自定义项5
+*
+* @param def5 自定义项5
+*/
+public void setDef5 ( String def5) {
+this.setAttributeValue( DocType.DEF5,def5);
+ } 
+
+/** 
+* 获取自定义项6
+*
+* @return 自定义项6
+*/
+public String getDef6 () {
+return (String) this.getAttributeValue( DocType.DEF6);
+ } 
+
+/** 
+* 设置自定义项6
+*
+* @param def6 自定义项6
+*/
+public void setDef6 ( String def6) {
+this.setAttributeValue( DocType.DEF6,def6);
+ } 
+
+/** 
+* 获取自定义项7
+*
+* @return 自定义项7
+*/
+public String getDef7 () {
+return (String) this.getAttributeValue( DocType.DEF7);
+ } 
+
+/** 
+* 设置自定义项7
+*
+* @param def7 自定义项7
+*/
+public void setDef7 ( String def7) {
+this.setAttributeValue( DocType.DEF7,def7);
+ } 
+
+/** 
+* 获取自定义项8
+*
+* @return 自定义项8
+*/
+public String getDef8 () {
+return (String) this.getAttributeValue( DocType.DEF8);
+ } 
+
+/** 
+* 设置自定义项8
+*
+* @param def8 自定义项8
+*/
+public void setDef8 ( String def8) {
+this.setAttributeValue( DocType.DEF8,def8);
+ } 
+
+/** 
+* 获取自定义项9
+*
+* @return 自定义项9
+*/
+public String getDef9 () {
+return (String) this.getAttributeValue( DocType.DEF9);
+ } 
+
+/** 
+* 设置自定义项9
+*
+* @param def9 自定义项9
+*/
+public void setDef9 ( String def9) {
+this.setAttributeValue( DocType.DEF9,def9);
+ } 
+
+/** 
+* 获取修改时间
+*
+* @return 修改时间
+*/
+public UFDateTime getModifiedtime () {
+return (UFDateTime) this.getAttributeValue( DocType.MODIFIEDTIME);
+ } 
+
+/** 
+* 设置修改时间
+*
+* @param modifiedtime 修改时间
+*/
+public void setModifiedtime ( UFDateTime modifiedtime) {
+this.setAttributeValue( DocType.MODIFIEDTIME,modifiedtime);
+ } 
+
+/** 
+* 获取修改人
+*
+* @return 修改人
+*/
+public String getModifier () {
+return (String) this.getAttributeValue( DocType.MODIFIER);
+ } 
+
+/** 
+* 设置修改人
+*
+* @param modifier 修改人
+*/
+public void setModifier ( String modifier) {
+this.setAttributeValue( DocType.MODIFIER,modifier);
+ } 
+
+/** 
+* 获取上级对象
+*
+* @return 上级对象
+*/
+public String getPid () {
+return (String) this.getAttributeValue( DocType.PID);
+ } 
+
+/** 
+* 设置上级对象
+*
+* @param pid 上级对象
+*/
+public void setPid ( String pid) {
+this.setAttributeValue( DocType.PID,pid);
+ } 
+
+/** 
+* 获取主键
+*
+* @return 主键
+*/
+public String getPk_doctype () {
+return this.pk_doctype;
+ } 
+
+/** 
+* 设置主键
+*
+* @param pk_doctype 主键
+*/
+public void setPk_doctype ( String pk_doctype) {
+this.pk_doctype=pk_doctype;
+ } 
+
+/** 
+* 获取集团
+*
+* @return 集团
+*/
+public String getPk_group () {
+return (String) this.getAttributeValue( DocType.PK_GROUP);
+ } 
+
+/** 
+* 设置集团
+*
+* @param pk_group 集团
+*/
+public void setPk_group ( String pk_group) {
+this.setAttributeValue( DocType.PK_GROUP,pk_group);
+ } 
+
+/** 
+* 获取组织
+*
+* @return 组织
+*/
+public String getPk_org () {
+return (String) this.getAttributeValue( DocType.PK_ORG);
+ } 
+
+/** 
+* 设置组织
+*
+* @param pk_org 组织
+*/
+public void setPk_org ( String pk_org) {
+this.setAttributeValue( DocType.PK_ORG,pk_org);
+ } 
+
+/** 
+* 获取组织版本
+*
+* @return 组织版本
+*/
+public String getPk_org_v () {
+return (String) this.getAttributeValue( DocType.PK_ORG_V);
+ } 
+
+/** 
+* 设置组织版本
+*
+* @param pk_org_v 组织版本
+*/
+public void setPk_org_v ( String pk_org_v) {
+this.setAttributeValue( DocType.PK_ORG_V,pk_org_v);
+ } 
+
+/** 
+* 获取时间戳
+*
+* @return 时间戳
+*/
+public UFDateTime getTs () {
+return this.ts;
+ } 
+
+/** 
+* 设置时间戳
+*
+* @param ts 时间戳
+*/
+public void setTs ( UFDateTime ts) {
+this.ts=ts;
+ } 
+
+/** 
+* 获取名称
+*
+* @return 名称
+*/
+public String getVcode () {
+return this.vcode;
+ } 
+
+/** 
+* 设置名称
+*
+* @param vcode 名称
+*/
+public void setVcode ( String vcode) {
+this.vcode=vcode;
+ } 
+
+/** 
+* 获取备注
+*
+* @return 备注
+*/
+public String getVmemo () {
+return (String) this.getAttributeValue( DocType.VMEMO);
+ } 
+
+/** 
+* 设置备注
+*
+* @param vmemo 备注
+*/
+public void setVmemo ( String vmemo) {
+this.setAttributeValue( DocType.VMEMO,vmemo);
+ } 
+
+
+  @Override
+  public IVOMeta getMetaData() {
+    return VOMetaFactory.getInstance().getVOMeta("docmng.DocType");
+  }
+}
